61b046017c9cadfb6686e4a17024c7ea3e16610c
Gustavo CR. da Silva <gustavo@lekas.com.br>
2022-05-04 16:34:00 -0300

    update

--- src/components/Charts/index.jsx
+++ src/components/Charts/index.jsx
@@ -19,17 +19,17 @@ export default function Charts() {
   let convertedCode, dataPoints, typeStart, typeSpan, typeStop;
   let typeData = [];
 
-  // const valor = `{type: 'start', timestamp: 1519862400000,select: ['min_response_time', 'max_response_time'],group: ['os', 'browser']}
-  //   {type: 'span', timestamp: 1519862400000, begin: 1519862400000, end: 1519862460000}
-  //   {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
-  //   {type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2}
-  //   {type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.2}
-  //   {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'firefox', min_response_time: 0.1, max_response_time: 1.0}
-  //   {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.2, max_response_time: 0.9}
-  //   {type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.0}
-  //   {type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'firefox', min_response_time: 0.2, max_response_time: 1.1}
-  //   {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.4}
-  //   {type: 'stop', timestamp: 1519862460000}`;
+  const valor = `{type: 'start', timestamp: 1519862400000,select: ['min_response_time', 'max_response_time'],group: ['os', 'browser']}
+  {type: 'span', timestamp: 1519862400000, begin: 1519862400000, end: 1519862460000}
+  {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+  {type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2}
+  {type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.2}
+  {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'firefox', min_response_time: 0.1, max_response_time: 1.0}
+  {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.2, max_response_time: 0.9}
+  {type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.0}
+  {type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'firefox', min_response_time: 0.2, max_response_time: 1.1}
+  {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.4}
+  {type: 'stop', timestamp: 1519862460000}`;
 
   function Converter() {
     convertedCode = "[" + text.split("\n") + "]";
@@ -179,6 +179,7 @@ export default function Charts() {
       />
       <div>
         <Button name="Generate Chart" onClick={GenerateChart} />
+        <Button name="Preset Value" onClick={() => setText(valor)} />
       </div>
     </div>
   );
